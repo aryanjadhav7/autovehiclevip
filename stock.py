6f8de4445993c9eed15ed1b35619aa11f7f7df8d
aryanjadhav7 <143037105+aryanjadhav7@users.noreply.github.com>
2024-02-29 12:04:58 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -19,7 +19,9 @@ def train_gp(X, y):
     gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
     gp.fit(X, y)
     return gp
-
+def create_data(prices):
+     # Calculate daily percent changes and return them
+    return np.diff(prices) / prices[:-1] * 100
 def main():
     ticker = input("Enter stock ticker: ")
     data = fetch_data(ticker)
@@ -28,9 +30,9 @@ def main():
         print(f"No data found for {ticker}.")
         return
 
-    X = np.arange(len(data)).reshape(-1, 1)
-    y = data['Close'].values
-
+    prices = create_data(data['Close'].values)
+    X = prices[:-1].reshape(-1,1)
+    y= prices[1:]
     scaler_X = StandardScaler()
     scaler_y = StandardScaler()
 
@@ -40,18 +42,29 @@ def main():
     gp = train_gp(X_scaled, y_scaled)
 
     # Predict future stock prices for the next 30 days
-    X_future = np.arange(len(data), len(data) + 30).reshape(-1, 1)
-    X_future_scaled = scaler_X.transform(X_future)
+    prediction = np.zeros(30)
+    prediction_prices = np.zeros(30)
+    prediction[0]=gp.predict(scaler_X.transform(prices[-1].reshape(-1,1)))
+    print(prediction[0])
+    prediction_prices[0]=data['Close'].values[-1]+((data['Close'].values[-1]*scaler_y.transform(prediction[0].reshape(-1,1))/100))
+    for i in range(1,30):
+        curr = prediction[i-1]
+        prediction[i]=gp.predict(scaler_X.transform(curr.reshape(-1,1)))
+        print(prediction[i])
+        prediction_prices[i]=prediction_prices[i-1]+((prediction_prices[i-1]*scaler_y.transform(prediction[i].reshape(-1,1)))/100)
 
-    y_pred_scaled = gp.predict(X_future_scaled)
-    y_pred = scaler_y.inverse_transform(y_pred_scaled.reshape(-1, 1)).flatten()
+    #X_future = np.arange(len(data), len(data) + 30).reshape(-1, 1)
+    #_future_scaled = scaler_y.transform(X_future)
+
+    #y_pred_scaled = gp.predict(X_future_scaled)
+    #y_pred = scaler_y.inverse_transform(y_pred_scaled.reshape(-1, 1)).flatten()
 
     # Plotting
     plt.clf()  # Clear the current figure
     plt.figure(figsize=(14, 7))
-    plt.plot(data.index, y, label='Historical Daily Closing Price')
+    plt.plot(data.index, data['Close'].values, label='Historical Daily Closing Price')
     future_dates = pd.date_range(start=data.index[-1] + pd.Timedelta(days=1), periods=30)
-    plt.plot(future_dates, y_pred, 'r-', label='Predicted Future Closing Price')
+    plt.plot(future_dates, prediction_prices, 'r-', label='Predicted Future Closing Price')
     plt.xlabel('Date')
     plt.ylabel('Stock Price (USD)')
     plt.title(f'Stock Price Prediction for {ticker}')
